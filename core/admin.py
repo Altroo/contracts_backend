20a10462b7d33dd81f48e0383c9d40a081a41154
Altroo <scorpion.yooy@gmail.com>
2026-04-07 11:30:40 +0100

    feat: unregister Group and Site models from admin if registered

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.contrib.admin.sites import NotRegistered
+from django.contrib.auth.models import Group
+from django.contrib.sites.models import Site
 
 from .models import CompanyConfig
 
@@ -8,3 +11,10 @@ class CompanyConfigAdmin(admin.ModelAdmin):
     list_display = ("company", "name", "forme_juridique", "representant")
     list_filter = ("company",)
     search_fields = ("name", "representant", "rc", "ice")
+
+
+for model in (Group, Site):
+    try:
+        admin.site.unregister(model)
+    except NotRegistered:
+        pass
